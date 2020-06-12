5b10e65d5ae7d69b13a529df95e6effa2fd3f488
arifpro <arif.swfu@outlook.com>
2020-06-12 20:04:35 +0800

    hex also done

--- hex.js
+++ hex.js
@@ -4,5 +4,14 @@ const btn = document.getElementById('btn')
 const color = document.querySelector('.color')
 
 btn.addEventListener('click', function() {
-    const hexColor = '#'
+    let hexColor = '#'
+
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()]
+    }
+
+    color.textContent = hexColor
+    document.body.style.backgroundColor = hexColor
 })
+
+const getRandomNumber = () => Math.floor(Math.random() * hex.length)
